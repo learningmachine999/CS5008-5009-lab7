bf06685d3d93c7d28181fef76b1dc765013a0b6c
learningmachine999 <131728093+learningmachine999@users.noreply.github.com>
2023-07-17 17:10:31 -0700

    Update part b.c

--- part b.c
+++ part b.c
@@ -1,75 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Function to sort the array using bubble sort algorithm
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j+1]
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+// Structure for a binary tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
     }
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
 }
 
-// Function to perform binary search on a sorted array
-int binarySearch(int arr[], int left, int right, int target) {
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
+// Breadth-first traversal
+void breadthFirstTraversal(struct Node* root) {
+    if (root == NULL)
+        return;
 
-        // Check if target is present at mid
-        if (arr[mid] == target)
-            return mid;
+    struct Node* queue[100]; // Assuming a maximum of 100 nodes
+    int front = 0;
+    int rear = 0;
 
-        // If target is greater, ignore the left half
-        if (arr[mid] < target)
-            left = mid + 1;
+    queue[rear++] = root;
 
-        // If target is smaller, ignore the right half
-        else
-            right = mid - 1;
+    while (front < rear) {
+        struct Node* node = queue[front++];
+        printf("%d ", node->data);
+
+        if (node->left != NULL)
+            queue[rear++] = node->left;
+
+        if (node->right != NULL)
+            queue[rear++] = node->right;
     }
 
-    // If we reach here, the element was not present
-    return -1;
+    printf("\n");
 }
 
 int main() {
-    int n;
+    // Create the binary tree
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    root->left->left->left = createNode(8);
+    root->left->left->right = createNode(9);
+    root->left->right->left = createNode(10);
+    root->left->right->right = createNode(11);
+    root->right->left = createNode(6);
+    root->right->right = createNode(7);
+    root->right->left->left = createNode(12);
+    root->right->left->right = createNode(13);
+    root->right->right->left = createNode(14);
+    root->right->right->right = createNode(15);
 
-    printf("Input array size: ");
-    scanf("%d", &n);
+    breadthFirstTraversal(root);
 
-    int arr[n];
-
-    printf("Input elements of the array:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    int target;
-    printf("Input target: ");
-    scanf("%d", &target);
-
-    printf("\nThe array:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    printf("\n\nThe target:\n%d\n\n", target);
-
-    // Sort the array using bubble sort
-    bubbleSort(arr, n);
-
-
-    printf("\n\nSearch result:\n");
-    int result = binarySearch(arr, 0, n - 1, target);
-    if (result == -1)
-        printf("-1\n");
-    else
-        printf("%d\n", result);
+    free(root);
 
     return 0;
 }
